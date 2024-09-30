241c1b01ef6b66fe6e7948f1cb3f9afaccd8a8cf
DannyBosonit <daot1993@gmail.com>
2024-09-30 11:44:40 -0500

    docs: Understanding Components & how content ends up on the screen

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -8,4 +8,11 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+/* El decorador '@component' añade metadatos a la clase 'AppComponent' y le dice a Angular
+que trate esta clase como un componente, este decorador recibe un objeto como argumento para configurar
+el componente, la propiedad 'selector' le dice a Angular que elementos debe buscar en el codigo HTML
+para reemplazarlos por el marcado contenido en el archivo HTML propio de este componente y que es asignado
+a la propiedad 'templateUrl' del objeto */
 export class AppComponent {}
+/* Esta clase 'AppComponent' es exportada de este archivo para ser utilizada en la función
+'bootstrapApplication()' del archivo 'main.ts' */
--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -1,5 +1,10 @@
+/* El código contenido en este archivo será el que se ejecutará cuando se cargue el sitio web */
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+/* Esta linea importa el componente que pide la función 'bootstrapApplication()' */
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+/* La función 'bootstrapApplication()' es proporcionada por el paquete Angular y pide como argumento
+un componente Angular que en este caso es 'AppComponent' para buscar la etiqueta HTML personalizada
+del componente 'app-root' y reemplazarla con el contenido del componente personalizado */
