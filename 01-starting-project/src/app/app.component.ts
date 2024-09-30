aa58a0c048c9b3cf7f69703a66bbf7b594812171
DannyBosonit <daot1993@gmail.com>
2024-09-30 15:10:38 -0500

    docs: Using the custom component

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -12,7 +12,8 @@ import { HeaderComponent } from './header/header.component';
 que trate esta clase como un componente, este decorador recibe un objeto como argumento para configurar
 el componente, la propiedad 'selector' le dice a Angular que elementos debe buscar en el codigo HTML
 para reemplazarlos por el marcado contenido en el archivo HTML propio de este componente y que es asignado
-a la propiedad 'templateUrl' del objeto */
+a la propiedad 'templateUrl' del objeto. La propiedad 'imports' del objeto recibe un array de importaciones
+que permite usar un componente dentro de otro declarando su nombre, en este caso 'HeaderComponent' */
 export class AppComponent {}
 /* Esta clase 'AppComponent' es exportada de este archivo para ser utilizada en la función
 'bootstrapApplication()' del archivo 'main.ts' */
