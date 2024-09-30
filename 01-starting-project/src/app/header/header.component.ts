e272a657fa7b4ccccf041c85bcec178b2ed0d099
DannyBosonit <daot1993@gmail.com>
2024-09-30 12:14:06 -0500

    docs: Configuring the custom Component

--- 01-starting-project/src/app/header/header.component.ts
+++ 01-starting-project/src/app/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 /* El decorador '@Component' es importado del paquete '@angular/core' de Angular */
 
-@Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
-})
+@Component(
+  {
+    selector: 'app-header',
+    standalone: true,
+    imports: [],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+  }
+  /* El nombre de la etiqueta 'app-header' es totalmente personalizable para poder diferenciarlo de la
+etiqueta 'header' que HTML maneja por defecto. La propiedad 'standalone' marca al componente como
+uno independiente, es desir que no se necesita de módulos para que este componente funcione en toda
+la aplicación */
+)
 export class HeaderComponent {}
