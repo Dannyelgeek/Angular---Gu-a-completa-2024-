86eaca65bb0b216c1210d9c1a9a2fdbaf9d6ef8b
DannyBosonit <daot1993@gmail.com>
2024-10-28 12:36:14 -0500

    Storing Data in a Component Class

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+/* Esta constante obtiene un número aleatorio basado en la cantidad de usuarios en el array 'DUMMY_USERS'. */
 
 @Component({
   selector: 'app-user',
@@ -6,4 +10,8 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  selecteduser = DUMMY_USERS[randomIndex];
+  /* En el cuerpo de una clase en TypeScript se pueden establecer propiedades sin usar 'let' o 'const' como se hace en
+  JavaScript, esta propiedad puede ser usada dentro de la plantilla HTML de este componente. */
+}
