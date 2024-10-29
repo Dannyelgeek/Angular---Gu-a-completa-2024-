07df97159553913ef53527fb14824b0fcefbe02e
DannyBosonit <daot1993@gmail.com>
2024-10-29 09:14:27 -0500

    Using etters For Computed Values

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -14,4 +14,12 @@ export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
   /* En el cuerpo de una clase en TypeScript se pueden establecer propiedades sin usar 'let' o 'const' como se hace en
   JavaScript, esta propiedad puede ser usada dentro de la plantilla HTML de este componente. */
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+  /* esto es un 'getter', que esencialmente es un método que devuelve un valor dinámico como
+  "'assets/users/' + selectedUser.avatar" para mantener la plantilla HTML lo más simple posible.
+
+  Un método es también utlilizado como una propiedad de la case dentro del componente.ts*/
 }
