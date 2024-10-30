3a687259237945f501cdc2a0746d0b38f57ab5ee
DannyBosonit <daot1993@gmail.com>
2024-10-30 09:13:16 -0500

    TypeScript: Working with potentially Undefined values & union types

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -8,5 +8,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) name!: string;
+  @Input() name?: string;
+  /* El signo de interrogación '?' le dice a TypeScript que la propiedad 'name' podría no estar configurada y que es
+  consiente de ello, puede que la propiedad no esté inicializada y no pasará nada.
+
+  Una alternativa al signo de interrogación sería definir un tipo 'unión' mediante el uso del signo de tubería '|'
+  de la siguiente manera:
+
+    @Input() name: string | undefined;
+
+  Esot le dice a TypeScript que el tipo de valor que puede ser almacenado en esta propiedad es de tipo 'string' ó
+  tipo 'undefined'  */
 }
