86ce55907cd99d7ca55a2bcd29255530de9f84eb
DannyBosonit <daot1993@gmail.com>
2024-11-01 10:38:18 -0500

    Managing the 'add task' dialog

--- 01-starting-project/src/app/tasks/add-task/add-task.component.ts
+++ 01-starting-project/src/app/tasks/add-task/add-task.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
   imports: [],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css'
+  styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
+  @Output() cancel = new EventEmitter<void>();
+  /* 'void' es un tipo especial de dato que hace que el 'EventEmitter' no emita ningún dato */
 
+  onCancel() {
+    this.cancel.emit();
+  }
 }
--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -63,4 +63,8 @@ export class TasksComponent {
   onStartAddTask() {
     this.isAddingTask = true;
   }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
 }
