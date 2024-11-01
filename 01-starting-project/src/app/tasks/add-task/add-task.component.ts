5a5f110d9e7ea4c99d463049831f507c18813581
DannyBosonit <daot1993@gmail.com>
2024-11-01 11:31:09 -0500

    Using directives & two-way-binding

--- 01-starting-project/src/app/tasks/add-task/add-task.component.ts
+++ 01-starting-project/src/app/tasks/add-task/add-task.component.ts
@@ -1,15 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
+  /* Se importa el módulo 'FormsModule' para poder registrar el uso de la directiva '[(ng-model)]=""' */
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
   @Output() cancel = new EventEmitter<void>();
   /* 'void' es un tipo especial de dato que hace que el 'EventEmitter' no emita ningún dato */
+
+  /* Las siguientes propiedades serán usadas para almacenar los valores que se introduzcan en el formulario usando
+  el 'two-way bindnind' */
+  enteredTitle = '';
+  enteredSumary = '';
+  enteredDate = '';
 
   onCancel() {
     this.cancel.emit();
