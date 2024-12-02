b715148bd0daca22f4d51c8c2f4179867d252758
DannyBosonit <daot1993@gmail.com>
2024-12-02 10:23:37 -0500

    Bootstraping Apps with Angular modules

--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -1,10 +1,22 @@
 /* El código contenido en este archivo será el que se ejecutará cuando se cargue el sitio web */
+
+/*
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-/* Esta linea importa el componente que pide la función 'bootstrapApplication()' */
 
+Esta linea importa el componente que pide la función 'bootstrapApplication()' */
+
+/*
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
-/* La función 'bootstrapApplication()' es proporcionada por el paquete Angular y pide como argumento
-un componente Angular que en este caso es 'AppComponent' para buscar la etiqueta HTML personalizada
+
+La función 'bootstrapApplication()' es proporcionada por el paquete Angular y pide como argumento
+un componente idependiente Angular que en este caso es 'AppComponent' para buscar la etiqueta HTML personalizada
 del componente 'app-root' y reemplazarla con el contenido del componente personalizado */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+/* Con el resultado de la función 'pletformBrowserDynamic()' se llama a la función 'bootstrapModule()' a la cual
+se le pasa una referencia a la clase del módulo principal de la aplicación que en este caso es 'AppModule'  */
